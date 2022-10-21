a4f596f778334c31354b0f9b3013988a0677d2b6
Than Hteik Aung <thanhtaikaung90@gmail.com>
2022-10-21 12:52:24 +0630

    reload data by using empty view pod if network failed CustomViewCreate v1.4

--- app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/veiws/viewpods/EmptyViewPod.kt
+++ app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/veiws/viewpods/EmptyViewPod.kt
@@ -10,7 +10,10 @@ class EmptyViewPod @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : RelativeLayout(context, attrs, defStyleAttr) {
 
+    private var mDelegate : Delegate? =null
+
     override fun onFinishInflate() {
+        setUpListener()
         super.onFinishInflate()
 
     }
@@ -23,5 +26,18 @@ class EmptyViewPod @JvmOverloads constructor(
             .into(ivEmptyImage)
     }
 
+    fun setDelegate(delegate:Delegate){
+        mDelegate = delegate
+    }
+
+    private fun setUpListener(){
+        btnTryAgain.setOnClickListener {
+            mDelegate?.onTapTryAgain()
+        }
+    }
+
+    interface Delegate {
+        fun onTapTryAgain()
+    }
 
 }
