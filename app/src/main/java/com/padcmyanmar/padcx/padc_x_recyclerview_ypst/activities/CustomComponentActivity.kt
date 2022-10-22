5dbe413c932c568caa87b4d990382f3459fd0f48
Than Hteik Aung <thanhtaikaung90@gmail.com>
2022-10-22 23:58:54 +0630

    Draw EmotionalFaceView(Custom View) CustomViewCreate v1.10

---
+++ app/src/main/java/com/padcmyanmar/padcx/padc_x_recyclerview_ypst/activities/CustomComponentActivity.kt
@@ -0,0 +1,12 @@
+package com.padcmyanmar.padcx.padc_x_recyclerview_ypst.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.padcmyanmar.padcx.padc_x_recyclerview_ypst.R
+
+class CustomComponentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_component)
+    }
+}
